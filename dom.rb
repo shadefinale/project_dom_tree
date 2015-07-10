4d165fefbc66c96129cd64d2d1866c31b6b9efab
mikebutts <mgbutts@gmail.com>
2015-07-09 20:31:24 -0400

    parse

--- dom.rb
+++ dom.rb
@@ -14,22 +14,38 @@ class DOMReader
   end
 
   def build_tree(file)
-    @file = File.readlines(file).each {|l| l.strip!}
-    depth = 0
-    start_element = 0
-    1.upto(@file.length-1) do |counter|
-      if @file[counter].scan(@@start_tag_regex).length > 0
-        start_element = counter if depth == 0
-        depth += 1
+    @file = File.readlines(file).each {|l| l.strip!}.join("")
+    print @file
+    # depth = 0
+    # start_element = 0
+    # 1.upto(@file.length-1) do |counter|
+    #   if @file[counter].scan(@@start_tag_regex).length > 0
+    #     start_element = counter if depth == 0
+    #     depth += 1
+    #   end
+    #   if @file[counter].scan(@@end_tag_regex).length > 0
+    #     depth -= 1
+    #     puts "Creating node from #{@file[start_element]} to #{@file[counter]}" if depth == 0
+    #   end
+    #   p @file[counter]
+    #   p depth
+    # end
+    stack = []
+    until @file.empty?
+      tag = get_tag 
+      if tag
+        new_node = parse_tag(tag)
+        new_node.parent = stack.peek
+        stack.peek.children << new_node
+        new_node.text += tag.pre_match
+        stack << new_node
+        @file = tag.post_match
+      else
+        stack.peek.text += tag.pre_match
+        stack.pop 
+        @file = tag.post_match
       end
-      if @file[counter].scan(@@end_tag_regex).length > 0
-        depth -= 1
-        puts "Creating node from #{@file[start_element]} to #{@file[counter]}" if depth == 0
-      end
-      p @file[counter]
-      p depth
     end
-
   end
 
   def create_node(start_point, end_point)
@@ -40,15 +56,14 @@ class DOMReader
   end
 
   def parse_tag(tag)
-    m = tag.scan(@@start_tag_regex)
-    element_type = m[0][0]
-    m = m[0]
-    m = m[1..-1] if m.length > 1
-    m = m.join(" ")
-    m = m.scan(@@attr_parse_regex)
-
+    m = tag.match(@@start_tag_regex)
+    element_type = m.captures[0]
+    attr  s = m.captures[1..-1]
+    attrs = attrs.join(" ")
+    attrs = attrs.scan(@@attr_parse_regex)
+ 
     options = {}
-    m.each do |opt|
+    attrs.each do |opt|
       options[opt[0].to_sym] = opt[1]
     end
 
